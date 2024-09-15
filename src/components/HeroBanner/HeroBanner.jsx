98d707b59b5448b0ecaa8a6a06574eaf63b9b789
Krunal M Pokar <129573891+krunal-99@users.noreply.github.com>
2024-09-15 20:08:27 +0530

    Hero Section Started

---
+++ src/components/HeroBanner/HeroBanner.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./HeroBanner.css";
+const HeroBanner = () => {
+  return (
+    <div className="banner">{/* <div className="container"></div> */}</div>
+  );
+};
+
+export default HeroBanner;
